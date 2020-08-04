ec266f20bf08057dff0f563d670e2d0822349b34
PyaePhyoAungKhant <pyaephyoaungkhant11@gmail.com>
2020-08-04 10:01:03 +0630

    first commit

---
+++ app/src/main/java/com/ppak/assigment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ppak.assigment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.exercise6)
+    }
+}
